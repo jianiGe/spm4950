ed3d1785b36047cf864fed23fa414be8c7b9c467
jianiGe <jianige@yahoo.com>
2024-05-13 06:47:59 +0200

    added first level specification and estimation

---
+++ 01 auditory/job/firstlevelest_job.m
@@ -0,0 +1,8 @@
+%-----------------------------------------------------------------------
+% Job saved on 10-May-2024 05:14:40 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.fmri_est.spmmat = {'C:\Users\jiani\Documents\MATLAB\spm4950\01 auditory\classical\SPM.mat'};
+matlabbatch{1}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
